b1dcb40fcd9bbf17a3df6a4c8fa9c17fd90c50da
Zack Blank <zack@zack949.com>
2021-01-29 21:24:13 -0500

    initial commit POC

---
+++ POC.py
@@ -0,0 +1,60 @@
+import os
+import logging
+from dotenv import load_dotenv
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+from telegram.ext import InlineQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+
+
+
+load_dotenv('key.env')
+token = os.getenv('token')
+updater = Updater(token= '1668967654:AAFxG8PvIYnsVAF2KNrUI32_whGMyC_-V18', use_context = True)
+
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text = 'I am a bot, please talk to me!')
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+def caps(update,context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Sorry, I do not understand that command.')
+
+start_handler = CommandHandler('start', start)
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+caps_handler = CommandHandler('caps', caps)
+inline_caps_handler = InlineQueryHandler(inline_caps)
+unknown_handler = MessageHandler(Filters.command, unknown)
+
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(caps_handler)
+dispatcher.add_handler(inline_caps_handler)
+dispatcher.add_handler(unknown_handler)
+
+updater.start_polling()
+#updater.stop()
