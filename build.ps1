84daa2af8b60fc6456acd4a389e6b8f5dd949799
Zack Blank <zack@zack949.com>
2021-01-30 09:16:06 -0500

    bug fix

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-key_values = "Token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+$key_values = "Token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 paralleldots = 'xxxxxxxxxxxxxxxxxxxxxxxx'"
 python -m pip install -r .\requirements.txt
-New-Item -Path . -Name "key.env" -ItemType "file" -Value key_values
+New-Item -Path . -Name "key.env" -ItemType "file" -Value $key_values
